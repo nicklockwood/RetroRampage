9c09c84bc9c4fd0241da17817784832b9e4f7d40
Jaleel Akbashev <jaleel.akbashev@icloud.com>
2019-07-13 21:04:44 +0200

    Made a separate view with two gesture recognizers

--- Source/Rampage/ViewController.swift
+++ Source/Rampage/ViewController.swift
@@ -26,44 +26,37 @@ private func loadTextures() -> Textures {
 }
 
 class ViewController: UIViewController {
-    private let imageView = UIImageView()
-    private let panGesture = UIPanGestureRecognizer()
+    
+    private let contentView: ContentView = ContentView()
+    
     private let textures = loadTextures()
     private var world = World(map: loadMap())
     private var lastFrameTime = CACurrentMediaTime()
-
+    
+    private var imageView: UIImageView {
+        return self.contentView.imageView
+    }
+    
+    override func loadView() {
+        self.view = contentView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUpImageView()
-
+        
         let displayLink = CADisplayLink(target: self, selector: #selector(update))
         displayLink.add(to: .main, forMode: .common)
-
-        view.addGestureRecognizer(panGesture)
+        
     }
-
-    private var inputVector: Vector {
-        switch panGesture.state {
-        case .began, .changed:
-            let translation = panGesture.translation(in: view)
-            var vector = Vector(x: Double(translation.x), y: Double(translation.y))
-            vector /= max(joystickRadius, vector.length)
-            panGesture.setTranslation(CGPoint(
-                x: vector.x * joystickRadius,
-                y: vector.y * joystickRadius
-            ), in: view)
-            return vector
-        default:
-            return Vector(x: 0, y: 0)
-        }
-    }
-
+    
     @objc func update(_ displayLink: CADisplayLink) {
         let timeStep = min(maximumTimeStep, displayLink.timestamp - lastFrameTime)
-        let inputVector = self.inputVector
-        let rotation = inputVector.x * world.player.turningSpeed * worldTimeStep
+        let leftInputVector = self.contentView.leftJoystickInputVector
+        let rightInputVector = self.contentView.rightJoystickInputVector
+        
+        let rotation = rightInputVector.x * world.player.turningSpeed * worldTimeStep
         let input = Input(
-            speed: -inputVector.y,
+            speed: -leftInputVector.y,
             rotation: Rotation(sine: sin(rotation), cosine: cos(rotation))
         )
         let worldSteps = (timeStep / worldTimeStep).rounded(.up)
@@ -71,23 +64,90 @@ class ViewController: UIViewController {
             world.update(timeStep: timeStep / worldSteps, input: input)
         }
         lastFrameTime = displayLink.timestamp
-
+        
         let width = Int(imageView.bounds.width), height = Int(imageView.bounds.height)
         var renderer = Renderer(width: width, height: height, textures: textures)
         renderer.draw(world)
-
+        
         imageView.image = UIImage(bitmap: renderer.bitmap)
     }
+    
+}
 
-    func setUpImageView() {
-        view.addSubview(imageView)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        imageView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-        imageView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+class ContentView: UIView {
+    
+    private(set) lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
         imageView.backgroundColor = .black
         imageView.layer.magnificationFilter = .nearest
+        return imageView
+    }()
+    
+    private let leftJoyStick = UIView()
+    private let rightJoystick = UIView()
+    
+    let leftGestureRecognizer = UIPanGestureRecognizer()
+    let rightGestureRecognizer = UIPanGestureRecognizer()
+    
+    var leftJoystickInputVector: Vector {
+        switch leftGestureRecognizer.state {
+        case .began, .changed:
+            let translation = leftGestureRecognizer.translation(in: self)
+            var vector = Vector(x: Double(translation.x), y: Double(translation.y))
+            vector /= max(joystickRadius, vector.length)
+            leftGestureRecognizer.setTranslation(CGPoint(
+                x: vector.x * joystickRadius,
+                y: vector.y * joystickRadius
+            ), in: self)
+            return vector
+        default:
+            return Vector(x: 0, y: 0)
+        }
+    }
+    
+    var rightJoystickInputVector: Vector {
+        switch rightGestureRecognizer.state {
+        case .began, .changed:
+            let translation = rightGestureRecognizer.translation(in: self)
+            var vector = Vector(x: Double(translation.x), y: Double(translation.y))
+            vector /= max(joystickRadius, vector.length)
+            rightGestureRecognizer.setTranslation(CGPoint(
+                x: vector.x * joystickRadius,
+                y: vector.y * joystickRadius
+            ), in: self)
+            return vector
+        default:
+            return Vector(x: 0, y: 0)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.addSubview(self.imageView)
+        
+        self.addSubview(self.leftJoyStick)
+        self.addSubview(self.rightJoystick)
+        
+        self.leftJoyStick.addGestureRecognizer(self.leftGestureRecognizer)
+        self.rightJoystick.addGestureRecognizer(self.rightGestureRecognizer)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let bounds = self.bounds
+        
+        self.imageView.frame = bounds
+        
+        self.leftJoyStick.frame = CGRect(origin: .zero,
+                                         size: CGSize(width: bounds.width / 2, height: bounds.height))
+        self.rightJoystick.frame = CGRect(origin: CGPoint(x: bounds.width / 2, y: 0),
+                                          size: CGSize(width: bounds.width / 2, height: bounds.height))
     }
 }
